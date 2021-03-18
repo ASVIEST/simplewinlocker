3cadafdc7979e24f8c12f5b80c7188cb197da178
ASVIEST <71895914+ASVIEST@users.noreply.github.com>
2021-03-18 20:37:26 +0300

    0.0.2

--- ui.py
+++ ui.py
@@ -6,62 +6,68 @@ from GlassBg import SetGlassBg
 import sys
 
 
-def window():
-	app = QApplication(sys.argv)
-	win = QMainWindow()
-	SetGlassBg(win)
-	screen_size = app.desktop().screenGeometry()
 
-	label = QtWidgets.QLabel(win)
-	label.setText("Windows locked")
-	label.setMinimumWidth(1024)
-	label.setMinimumHeight(128)
-
-	font = QtGui.QFont()
-	font.setFamily("Verdana")
-	pointsize = 64
-	font.setPointSize(pointsize)
-	label.setFont(font)
-
-	labelcenter = (screen_size.width() / 2 - label.width() / 2, screen_size.height() / 2 - label.height() / 2) #center of label
-	labelcenterdown = (screen_size.width() / 2 - label.width() / 2, screen_size.height() - label.height() / 2 - pointsize)
-	label.setStyleSheet("color: rgba(11, 11, 11, 125);")
-	label.move(*labelcenterdown)
+class SimpleWinlockerUI(QMainWindow):
+    def __init__(self, screen_size):
+        super(SimpleWinlockerUI, self).__init__()
+        self.pointsize = 64
+        self.screen_size = screen_size
+        print(self.screen_size)
+        self.initUI()
 
 
-	lineEdit = QtWidgets.QLineEdit(win)
-	lineEdit.setMinimumWidth(1024)
-	lineEdit.setMinimumHeight(128)
-	lineEdit.move(screen_size.width() / 2 - lineEdit.width() / 2, screen_size.height() / 2 - lineEdit.height() / 2)
+    def initUI(self):
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(self.pointsize)
 
-	
-	lineEdit.setStyleSheet("background-color: transparent;\n"
-	"color: rgba(11, 11, 11, 125);\n"
-	"\n"
-	"selection-color: rgb(255, 255, 255, 2);\n"
-	"selection-background-color: rgba(91, 91, 91, .3);\n"
-	"\n"
-	"border: 8px rgba(11, 11, 11, 125);\n"
-	"border-radius: 16px;"
-	"\n"
-	"text-align: center;")
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Windows locked")
+        self.label.setMinimumWidth(1024)
+        self.label.setMinimumHeight(128)
+        self.label.setStyleSheet("color: rgba(11, 11, 11, 125);")
+        self.label.move(self.screen_size.width() / 2 - self.label.width() / 2, self.screen_size.height() - self.label.height() / 2 - self.pointsize)
+        self.label.setFont(font)
 
-	lineEdit.setFont(font)
+        self.lineEdit = QtWidgets.QLineEdit(self)
+        self.lineEdit.setMinimumWidth(1024)
+        self.lineEdit.setMinimumHeight(128)
+        self.lineEdit.move(self.screen_size.width() / 2 - self.lineEdit.width() / 2, self.screen_size.height() / 2 - self.lineEdit.height() / 2)
+        self.lineEdit.setStyleSheet("background-color: transparent;\n"
+        "color: rgba(11, 11, 11, 125);\n"
+        "\n"
+        "selection-color: rgb(255, 255, 255, 2);\n"
+        "selection-background-color: rgba(91, 91, 91, .3);\n"
+        "\n"
+        "border: 8px rgba(11, 11, 11, 125);\n"
+        "border-radius: 16px;"
+        "\n"
+        "text-align: center;")
+        self.lineEdit.setFont(font)
+
+        self.unlock_btn = QtWidgets.QPushButton(self)
+        self.unlock_btn.setMinimumWidth(512)
+        self.unlock_btn.setMinimumHeight(128)
+        self.unlock_btn.move(self.screen_size.width() / 2 - self.unlock_btn.width() / 2, self.screen_size.height() - self.unlock_btn.height() / 2 - 64 - (self.screen_size.height() - self.label.height() / 2 - 64) / 4)
+        self.unlock_btn.setText("Unlock")
+        self.unlock_btn.setFont(font)
+        self.unlock_btn.setStyleSheet("color: rgba(11, 11, 11, 125); background-color: rgba(0, 0, 0, .04); border-radius: 32px;")
+        self.unlock_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+
+        # label.setMinimumWidth(1024)
+        # label.setMinimumHeight(128)
+    def update(self):
+        self.label.adjustSize()
 
 
 
-	unlock_btn = QtWidgets.QPushButton(win)
-	unlock_btn.setMinimumWidth(512)
-	unlock_btn.setMinimumHeight(128)
-	unlock_btn.move(screen_size.width() / 2 - unlock_btn.width() / 2, screen_size.height() - unlock_btn.height() / 2 - 64 - (screen_size.height() - label.height() / 2 - 64) / 4)
-	unlock_btn.setText("Unlock")
-	unlock_btn.setFont(font)
-	unlock_btn.setStyleSheet("color: rgba(11, 11, 11, 125); background-color: rgba(0, 0, 0, .04); border-radius: 32px;")
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
 
+    win = SimpleWinlockerUI(app.desktop().screenGeometry())
+    SetGlassBg(win)
 
-
-	win.showFullScreen()
-	sys.exit(app.exec_())
-
-window()
+    win.showFullScreen()
+    sys.exit(app.exec_())
